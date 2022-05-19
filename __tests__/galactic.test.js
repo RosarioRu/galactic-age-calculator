9bf39ba946d499adc180c1d17a8f55b19bf3e98b
RosarioRu <mariarruval@gmail.com>
2022-05-19 16:09:35 -0700

    Update name of and in the files for the project from 'template' to 'galactic'.

---
+++ __tests__/galactic.test.js
@@ -0,0 +1,1 @@
+import TemplateClassName from '../src/template.js';
--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(), 
     new HtmlWebpackPlugin({
-      title: 'TEMPLATE',
+      title: 'galactic-age-calculator',
       template: './src/index.html',
       inject: 'body'
     })
