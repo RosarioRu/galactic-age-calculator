80acc3862aac5752737c5f6de2b40b9734ff9795
RosarioRu <mariarruval@gmail.com>
2022-05-19 22:32:29 -0700

    Refactor test for 'GalacticAges' class by establishing beforeEach() instantiation to be reused.

--- __tests__/galactic.test.js
+++ __tests__/galactic.test.js
@@ -1,29 +1,26 @@
 import GalacticAges from './../src/galactic.js';
 
 describe('GalacticAges', () => {
+
+  let lunaFromTheMilkyWay;
+  beforeEach(() => {
+    lunaFromTheMilkyWay = new GalacticAges("Luna", 35);
+  });
   test('should create an object with two properties - userName and userAge', () => {
-    const lunaFromTheMilkyWay = new GalacticAges("Luna", 35);
     expect(lunaFromTheMilkyWay.userName).toEqual("Luna");
     expect(lunaFromTheMilkyWay.userAge).toEqual(35);
   });
 
   test('method should add another property to the object for the age of the user in Mercury', () => {
-    const lunaFromTheMilkyWay = new GalacticAges("Luna", 35);
     lunaFromTheMilkyWay.calculateAges();
     expect(lunaFromTheMilkyWay.ageInMercury).toEqual(145);
   });
 
   test('method should add three other properties to the object, the age of the user in Venus, Mars and Jupiter', () => {
-    const lunaFromTheMilkyWay = new GalacticAges("Luna", 35);
     lunaFromTheMilkyWay.calculateAges();
     expect(lunaFromTheMilkyWay.ageInVenus).toEqual(56);
     expect(lunaFromTheMilkyWay.ageInMars).toEqual(18);
     expect(lunaFromTheMilkyWay.ageInJupiter).toEqual(2);
-    
   });
   
-
-  
-
-
 });
