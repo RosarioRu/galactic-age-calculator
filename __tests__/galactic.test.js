4b0953c4e546a09ab8418e5c92f3017ec3615348
RosarioRu <mariarruval@gmail.com>
2022-05-19 19:07:01 -0700

    Add first test for constructor function inside class 'GalacticAges'

--- __tests__/galactic.test.js
+++ __tests__/galactic.test.js
@@ -1,1 +1,10 @@
-import GalacticAge from './../src/galactic.js';
+import GalacticAges from './../src/galactic.js';
+
+describe('GalacticAges', () => {
+
+  test('should create an object with two properties - userName and userAge', () => {
+    const userGalacticAges = new GalacticAges("Luna", 35);
+    expect(userGalacticAges.userName).toEqual("Luna");
+    expect(userGalacticAges.userAge).toEqual(35);
+  });
+});
