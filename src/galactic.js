6e08811f1c4c95f964909b8e4094c7ba1301094f
RosarioRu <mariarruval@gmail.com>
2022-05-19 19:12:57 -0700

    Add constructor function in class 'GalacticAges' that passes the first test.

--- src/galactic.js
+++ src/galactic.js
@@ -1,14 +1,10 @@
 //business logic with class example below:
 
 export default class GalacticAges {
-
-
-  //example of a constructor function:
-  // constructor (intelligence, charm) {
-  //   this.intelligence = intelligence;
-  //   this.charm = charm;
-  //   this.health = 100;
-  // }
+  constructor (userName, userAge) {
+    this.userName = userName;
+    this.userAge = userAge;
+  }
 
   //example of a method function:
   // templateExample(number) {
