bf255ed3799f4c8f44f11065f098efa4eaf00ed7
RosarioRu <mariarruval@gmail.com>
2022-05-22 06:45:45 -0700

    Refactors/Updates the first 3 tests and corresponding functions to store user's age in different planets within an object inside the main object.

--- __tests__/galactic.test.js
+++ __tests__/galactic.test.js
@@ -7,78 +7,78 @@ describe('GalacticAges', () => {
     lunaFromTheMilkyWay = new GalacticAges("Luna", 35);
   });
 
-  test('should create an object with two properties - userName and userEarthAge', () => {
+  test('should create an object with two properties - userName and age(inside earthInfo)', () => {
     expect(lunaFromTheMilkyWay.userName).toEqual("Luna");
-    expect(lunaFromTheMilkyWay.userEarthAge).toEqual([35]);
+    expect(lunaFromTheMilkyWay.earthInfo.age).toEqual(35);
   });
 
-  test('method should add another property to the object for the age of the user in Mercury inside an array', () => {
+  test('method should add another property to the object for the age of the user in Mercury inside an object', () => {
     lunaFromTheMilkyWay.calculateAges();
-    expect(lunaFromTheMilkyWay.ageInMercury).toEqual([145.83]);
+    expect(lunaFromTheMilkyWay.mercuryInfo.age).toEqual(145.83);
   });
 
-  test('method should add three other properties to the object, the age of the user in Venus, Mars and Jupiter inside arrays', () => {
+  test('method should add three other properties to the object, the age of the user in Venus, Mars and Jupiter inside planetInfo objects', () => {
     lunaFromTheMilkyWay.calculateAges();
-    expect(lunaFromTheMilkyWay.ageInVenus).toEqual([56.45]);
-    expect(lunaFromTheMilkyWay.ageInMars).toEqual([18.62]);
-    expect(lunaFromTheMilkyWay.ageInJupiter).toEqual([2.95]);
+    expect(lunaFromTheMilkyWay.venusInfo.age).toEqual(56.45);
+    expect(lunaFromTheMilkyWay.marsInfo.age).toEqual(18.62);
+    expect(lunaFromTheMilkyWay.jupiterInfo.age).toEqual(2.95);
   });
 
-  test('method should assign another property to the object called healthStatus and in this case return "healthy." It should also calculate years left on each planet based on "healthy" healthStatus and set these as second elements in array for each planet age', () => {
-    const nonSmoker = true;
-    const exercise = true;
-    const healthyDiet = true;
-    lunaFromTheMilkyWay.calculateAges();
-    lunaFromTheMilkyWay.determineLifeExpectancy(nonSmoker, exercise, healthyDiet);
-    expect(lunaFromTheMilkyWay.healthStatus).toEqual("healthy");
-    expect(lunaFromTheMilkyWay.userEarthAge[1]).toEqual(47);
-    expect(lunaFromTheMilkyWay.ageInMercury[1]).toEqual(196);
-    expect(lunaFromTheMilkyWay.ageInVenus[1]).toEqual(76);
-    expect(lunaFromTheMilkyWay.ageInMars[1]).toEqual(25);
-    expect(lunaFromTheMilkyWay.ageInJupiter[1]).toEqual(4);
-  });
+  // test('method should assign another property to the object called healthStatus and in this case return "healthy." It should also calculate years left on each planet based on "healthy" healthStatus and set these as second elements in array for each planet age', () => {
+  //   const nonSmoker = true;
+  //   const exercise = true;
+  //   const healthyDiet = true;
+  //   lunaFromTheMilkyWay.calculateAges();
+  //   lunaFromTheMilkyWay.determineLifeExpectancy(nonSmoker, exercise, healthyDiet);
+  //   expect(lunaFromTheMilkyWay.healthStatus).toEqual("healthy");
+  //   expect(lunaFromTheMilkyWay.userEarthAge[1]).toEqual(47);
+  //   expect(lunaFromTheMilkyWay.ageInMercury[1]).toEqual(196);
+  //   expect(lunaFromTheMilkyWay.ageInVenus[1]).toEqual(76);
+  //   expect(lunaFromTheMilkyWay.ageInMars[1]).toEqual(25);
+  //   expect(lunaFromTheMilkyWay.ageInJupiter[1]).toEqual(4);
+  // });
 
-  test('method should assign another property to the object called healthStatus and in this case return "unhealthy." It should also calculate years left on each planet based on "unhealthy" healthStatus and set these as second elements in array for each planet age', () => {
-    const nonSmoker = false;
-    const exercise = false;
-    const healthyDiet = false;
-    lunaFromTheMilkyWay.calculateAges();
-    lunaFromTheMilkyWay.determineLifeExpectancy(nonSmoker, exercise, healthyDiet);
-    expect(lunaFromTheMilkyWay.healthStatus).toEqual("unhealthy");
-    expect(lunaFromTheMilkyWay.userEarthAge[1]).toEqual(41);
-    expect(lunaFromTheMilkyWay.ageInMercury[1]).toEqual(171);
-    expect(lunaFromTheMilkyWay.ageInVenus[1]).toEqual(66);
-    expect(lunaFromTheMilkyWay.ageInMars[1]).toEqual(22);
-    expect(lunaFromTheMilkyWay.ageInJupiter[1]).toEqual(3);
-  });
+  // test('method should assign another property to the object called healthStatus and in this case return "unhealthy." It should also calculate years left on each planet based on "unhealthy" healthStatus and set these as second elements in array for each planet age', () => {
+  //   const nonSmoker = false;
+  //   const exercise = false;
+  //   const healthyDiet = false;
+  //   lunaFromTheMilkyWay.calculateAges();
+  //   lunaFromTheMilkyWay.determineLifeExpectancy(nonSmoker, exercise, healthyDiet);
+  //   expect(lunaFromTheMilkyWay.healthStatus).toEqual("unhealthy");
+  //   expect(lunaFromTheMilkyWay.userEarthAge[1]).toEqual(41);
+  //   expect(lunaFromTheMilkyWay.ageInMercury[1]).toEqual(171);
+  //   expect(lunaFromTheMilkyWay.ageInVenus[1]).toEqual(66);
+  //   expect(lunaFromTheMilkyWay.ageInMars[1]).toEqual(22);
+  //   expect(lunaFromTheMilkyWay.ageInJupiter[1]).toEqual(3);
+  // });
 
-  test('method should assign another property to the object called healthStatus and in this case return "standard"', () => {
-    const nonSmoker = true;
-    const exercise = false;
-    const healthyDiet = false;
-    lunaFromTheMilkyWay.calculateAges();
-    lunaFromTheMilkyWay.determineLifeExpectancy(nonSmoker, exercise, healthyDiet);
-    expect(lunaFromTheMilkyWay.healthStatus).toEqual("standard");
-    expect(lunaFromTheMilkyWay.userEarthAge[1]).toEqual(44);
-    expect(lunaFromTheMilkyWay.ageInMercury[1]).toEqual(183);
-    expect(lunaFromTheMilkyWay.ageInVenus[1]).toEqual(71);
-    expect(lunaFromTheMilkyWay.ageInMars[1]).toEqual(23);
-    expect(lunaFromTheMilkyWay.ageInJupiter[1]).toEqual(4);
-  });  
+  // test('method should assign another property to the object called healthStatus and in this case return "standard"', () => {
+  //   const nonSmoker = true;
+  //   const exercise = false;
+  //   const healthyDiet = false;
+  //   lunaFromTheMilkyWay.calculateAges();
+  //   lunaFromTheMilkyWay.determineLifeExpectancy(nonSmoker, exercise, healthyDiet);
+  //   expect(lunaFromTheMilkyWay.healthStatus).toEqual("standard");
+  //   expect(lunaFromTheMilkyWay.userEarthAge[1]).toEqual(44);
+  //   expect(lunaFromTheMilkyWay.ageInMercury[1]).toEqual(183);
+  //   expect(lunaFromTheMilkyWay.ageInVenus[1]).toEqual(71);
+  //   expect(lunaFromTheMilkyWay.ageInMars[1]).toEqual(23);
+  //   expect(lunaFromTheMilkyWay.ageInJupiter[1]).toEqual(4);
+  // });  
 
-  test('it should return a string that contains years above life-expectancy when users age surpasses it: "Over estimated life-expectancy by x years" and set this as the second element in the array containing users age in each planet', () => {
-    let oldLuna = new GalacticAges("Luna", "100");
-    const nonSmoker = true;
-    const exercise = true;
-    const healthyDiet = true;
-    oldLuna.calculateAges();
-    oldLuna.determineLifeExpectancy(nonSmoker, exercise, healthyDiet);
-    expect(oldLuna.healthStatus).toEqual("healthy");
-    expect(oldLuna.earthInfo[1]).toEqual("Over estimated life-expectancy by " + 18 + " years");
-    expect(oldLuna.ageInMercury[1]).toEqual("Over estimated life-expectancy by " + 75 + " years");
-    expect(oldLuna.ageInVenus[1]).toEqual("Over estimated life-expectancy by " + 29 + " years");
-    expect(oldLuna.ageInMars[1]).toEqual("Over estimated life-expectancy by " + 10 + " years");
-    expect(oldLuna.ageInJupiter[1]).toEqual("Over estimated life-expectancy by " + 2 + " years");
-  });
+  // test('it should return a string that contains years above life-expectancy when users age surpasses it: "Over estimated life-expectancy by x years" and set this as the second element in the array containing users age in each planet', () => {
+  //   let oldLuna = new GalacticAges("Luna", "100");
+  //   const nonSmoker = true;
+  //   const exercise = true;
+  //   const healthyDiet = true;
+  //   oldLuna.calculateAges();
+  //   oldLuna.determineLifeExpectancy(nonSmoker, exercise, healthyDiet);
+  //   expect(oldLuna.healthStatus).toEqual("healthy");
+  //   expect(oldLuna.earthInfo[1]).toEqual("Over estimated life-expectancy by " + 18 + " years");
+  //   expect(oldLuna.ageInMercury[1]).toEqual("Over estimated life-expectancy by " + 75 + " years");
+  //   expect(oldLuna.ageInVenus[1]).toEqual("Over estimated life-expectancy by " + 29 + " years");
+  //   expect(oldLuna.ageInMars[1]).toEqual("Over estimated life-expectancy by " + 10 + " years");
+  //   expect(oldLuna.ageInJupiter[1]).toEqual("Over estimated life-expectancy by " + 2 + " years");
+  // });
 
 })
--- src/galactic.js
+++ src/galactic.js
@@ -8,47 +8,49 @@ export default class GalacticAges {
 
   calculateAges() {
     const earthAge = this.earthInfo.age;
-    this.ageInMercury = [Math.round(100*earthAge/0.24)/100];
-    this.ageInVenus = [Math.round(100*earthAge/0.62)/100];
-    this.ageInMars = [Math.round(100*earthAge/1.88)/100];
-    this.ageInJupiter = [Math.round(100*earthAge/11.86)/100];
+    this.mercuryInfo = {age: Math.round(100*earthAge/0.24)/100};
+    // this.mercuryInfo = {};
+    // this["mercuryInfo"].age = Math.round(100*earthAge/0.24)/100;
+    this.venusInfo = {age: Math.round(100*earthAge/0.62)/100};
+    this.marsInfo = {age: Math.round(100*earthAge/1.88)/100};
+    this.jupiterInfo = {age: Math.round(100*earthAge/11.86)/100};
   }
 
-  determineLifeExpectancy(nonSmoker, exercises, healthyDiet) {
-    const averageEarthLifeExpectancy = 78.79;
-    if ((nonSmoker === true) && (exercises === true) && (healthyDiet === true)) {
-      this.healthStatus = "healthy";
-      this.earthInfo[1] = Math.round(averageEarthLifeExpectancy+3 - this.earthInfo[0]);
-        if (this.earthInfo[1] >= 0) {
-          this.ageInMercury[1] = Math.round(this.earthInfo[1]/0.24);
-          this.ageInVenus[1] = Math.round(this.earthInfo[1]/0.62);
-          this.ageInMars[1] = Math.round(this.earthInfo[1]/1.88);
-          this.ageInJupiter[1] = Math.round(this.earthInfo[1]/11.86);
-        } else {
-          const earthYrsPastLifeExpectancy = Math.abs(this.earthInfo[1]);
-          this.earthInfo[1] = ("Over estimated life-expectancy by " + Math.round(earthYrsPastLifeExpectancy) + " years");
-          this.ageInMercury[1] = "Over estimated life-expectancy by " + Math.round(earthYrsPastLifeExpectancy/0.24) + " years";
-          this.ageInVenus[1] = "Over estimated life-expectancy by " + Math.round(earthYrsPastLifeExpectancy/0.62) + " years";
-          ;
-          this.ageInMars[1] = "Over estimated life-expectancy by " + Math.round(earthYrsPastLifeExpectancy/1.88) + " years";
-          this.ageInJupiter[1] = "Over estimated life-expectancy by " + Math.round(earthYrsPastLifeExpectancy/11.86) + " years";
-        }
-    } else if (nonSmoker===false && exercises===false && healthyDiet ===false) {
-      this.healthStatus = "unhealthy";
-      this.earthInfo[1] = Math.round(averageEarthLifeExpectancy-3 - this.earthInfo[0]);
-      this.ageInMercury[1] = Math.round(this.earthInfo[1]/0.24);
-      this.ageInVenus[1] = Math.round(this.earthInfo[1]/0.62);
-      this.ageInMars[1] = Math.round(this.earthInfo[1]/1.88);
-      this.ageInJupiter[1] = Math.round(this.earthInfo[1]/11.86);
-    } else {
-      this.healthStatus = "standard";
-      this.earthInfo[1] = Math.round(averageEarthLifeExpectancy - this.earthInfo[0]);
-      this.ageInMercury[1] = Math.round(this.earthInfo[1]/0.24);
-      this.ageInVenus[1] = Math.round(this.earthInfo[1]/0.62);
-      this.ageInMars[1] = Math.round(this.earthInfo[1]/1.88);
-      this.ageInJupiter[1] = Math.round(this.earthInfo[1]/11.86);
-    }
-  }
+  // determineLifeExpectancy(nonSmoker, exercises, healthyDiet) {
+  //   const averageEarthLifeExpectancy = 78.79;
+  //   if ((nonSmoker === true) && (exercises === true) && (healthyDiet === true)) {
+  //     this.healthStatus = "healthy";
+  //     this.earthInfo[1] = Math.round(averageEarthLifeExpectancy+3 - this.earthInfo[0]);
+  //       if (this.earthInfo[1] >= 0) {
+  //         this.ageInMercury[1] = Math.round(this.earthInfo[1]/0.24);
+  //         this.ageInVenus[1] = Math.round(this.earthInfo[1]/0.62);
+  //         this.ageInMars[1] = Math.round(this.earthInfo[1]/1.88);
+  //         this.ageInJupiter[1] = Math.round(this.earthInfo[1]/11.86);
+  //       } else {
+  //         const earthYrsPastLifeExpectancy = Math.abs(this.earthInfo[1]);
+  //         this.earthInfo[1] = ("Over estimated life-expectancy by " + Math.round(earthYrsPastLifeExpectancy) + " years");
+  //         this.ageInMercury[1] = "Over estimated life-expectancy by " + Math.round(earthYrsPastLifeExpectancy/0.24) + " years";
+  //         this.ageInVenus[1] = "Over estimated life-expectancy by " + Math.round(earthYrsPastLifeExpectancy/0.62) + " years";
+  //         ;
+  //         this.ageInMars[1] = "Over estimated life-expectancy by " + Math.round(earthYrsPastLifeExpectancy/1.88) + " years";
+  //         this.ageInJupiter[1] = "Over estimated life-expectancy by " + Math.round(earthYrsPastLifeExpectancy/11.86) + " years";
+  //       }
+  //   } else if (nonSmoker===false && exercises===false && healthyDiet ===false) {
+  //     this.healthStatus = "unhealthy";
+  //     this.earthInfo[1] = Math.round(averageEarthLifeExpectancy-3 - this.earthInfo[0]);
+  //     this.ageInMercury[1] = Math.round(this.earthInfo[1]/0.24);
+  //     this.ageInVenus[1] = Math.round(this.earthInfo[1]/0.62);
+  //     this.ageInMars[1] = Math.round(this.earthInfo[1]/1.88);
+  //     this.ageInJupiter[1] = Math.round(this.earthInfo[1]/11.86);
+  //   } else {
+  //     this.healthStatus = "standard";
+  //     this.earthInfo[1] = Math.round(averageEarthLifeExpectancy - this.earthInfo[0]);
+  //     this.ageInMercury[1] = Math.round(this.earthInfo[1]/0.24);
+  //     this.ageInVenus[1] = Math.round(this.earthInfo[1]/0.62);
+  //     this.ageInMars[1] = Math.round(this.earthInfo[1]/1.88);
+  //     this.ageInJupiter[1] = Math.round(this.earthInfo[1]/11.86);
+  //   }
+  // }
 
  
 }
