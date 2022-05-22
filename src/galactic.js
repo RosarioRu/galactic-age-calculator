6ac2944f1d2624bb6b4fecafbc989a9cec128595
RosarioRu <mariarruval@gmail.com>
2022-05-21 21:26:45 -0700

    Pass test for returning years over life expectancy as a positive number.

--- src/galactic.js
+++ src/galactic.js
@@ -19,10 +19,20 @@ export default class GalacticAges {
     if ((nonSmoker === true) && (exercises === true) && (healthyDiet === true)) {
       this.healthStatus = "healthy";
       this.userEarthAge[1] = Math.round(averageEarthLifeExpectancy+3 - this.userEarthAge[0]);
-      this.ageInMercury[1] = Math.round(this.userEarthAge[1]/0.24);
-      this.ageInVenus[1] = Math.round(this.userEarthAge[1]/0.62);
-      this.ageInMars[1] = Math.round(this.userEarthAge[1]/1.88);
-      this.ageInJupiter[1] = Math.round(this.userEarthAge[1]/11.86);
+        if (this.userEarthAge[1] >= 0) {
+          this.ageInMercury[1] = Math.round(this.userEarthAge[1]/0.24);
+          this.ageInVenus[1] = Math.round(this.userEarthAge[1]/0.62);
+          this.ageInMars[1] = Math.round(this.userEarthAge[1]/1.88);
+          this.ageInJupiter[1] = Math.round(this.userEarthAge[1]/11.86);
+        } else {
+          const earthYrsPastLifeExpectancy = Math.abs(this.userEarthAge[1]);
+          this.userEarthAge[1] = ("Over estimated life-expectancy by " + Math.round(earthYrsPastLifeExpectancy) + " years");
+          this.ageInMercury[1] = "Over estimated life-expectancy by " + Math.round(earthYrsPastLifeExpectancy/0.24) + " years";
+          this.ageInVenus[1] = "Over estimated life-expectancy by " + Math.round(earthYrsPastLifeExpectancy/0.62) + " years";
+          ;
+          this.ageInMars[1] = "Over estimated life-expectancy by " + Math.round(earthYrsPastLifeExpectancy/1.88) + " years";
+          this.ageInJupiter[1] = "Over estimated life-expectancy by " + Math.round(earthYrsPastLifeExpectancy/11.86) + " years";
+        }
     } else if (nonSmoker===false && exercises===false && healthyDiet ===false) {
       this.healthStatus = "unhealthy";
       this.userEarthAge[1] = Math.round(averageEarthLifeExpectancy-3 - this.userEarthAge[0]);
