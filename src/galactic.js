acb2a1f251afd402a095544028c4e83df1853fe5
RosarioRu <mariarruval@gmail.com>
2022-05-22 07:14:18 -0700

    Pass test for 'unhealthy' status

--- src/galactic.js
+++ src/galactic.js
@@ -33,13 +33,13 @@ export default class GalacticAges {
           this.marsInfo.yearsLeft = "Over estimated life-expectancy by " + Math.round(earthYrsPastLifeExpectancy/1.88) + " years";
           this.jupiterInfo.yearsLeft= "Over estimated life-expectancy by " + Math.round(earthYrsPastLifeExpectancy/11.86) + " years";
         }
-  //   } else if (nonSmoker===false && exercises===false && healthyDiet ===false) {
-  //     this.healthStatus = "unhealthy";
-  //     this.earthInfo[1] = Math.round(averageEarthLifeExpectancy-3 - this.earthInfo[0]);
-  //     this.ageInMercury[1] = Math.round(this.earthInfo[1]/0.24);
-  //     this.ageInVenus[1] = Math.round(this.earthInfo[1]/0.62);
-  //     this.ageInMars[1] = Math.round(this.earthInfo[1]/1.88);
-  //     this.ageInJupiter[1] = Math.round(this.earthInfo[1]/11.86);
+    } else if (nonSmoker===false && exercises===false && healthyDiet ===false) {
+      this.healthStatus = "unhealthy";
+      this.earthInfo.yearsLeft = Math.round(averageEarthLifeExpectancy-3 - this.earthInfo.age);
+      this.mercuryInfo.yearsLeft = Math.round(this.earthInfo.yearsLeft/0.24);
+      this.venusInfo.yearsLeft = Math.round(this.earthInfo.yearsLeft/0.62);
+      this.marsInfo.yearsLeft = Math.round(this.earthInfo.yearsLeft/1.88);
+      this.jupiterInfo.yearsLeft = Math.round(this.earthInfo.yearsLeft/11.86);
   //   } else {
   //     this.healthStatus = "standard";
   //     this.earthInfo[1] = Math.round(averageEarthLifeExpectancy - this.earthInfo[0]);
