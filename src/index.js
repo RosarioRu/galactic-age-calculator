f88119ce5c3ed545f50f4a06765c2675c48eb154
RosarioRu <mariarruval@gmail.com>
2022-05-22 08:18:40 -0700

    Add comment for user data that will be needed when UI is build.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,15 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import GalacticAge from './galactic.js';
+import GalacticAges from './galactic.js';
 
 $(document).ready(function() {
   //UI logic goes here!
-  });
+  //obtain user data for the following variables: 
+  // name (string)
+  // age (integer)
+  // nonSmoker (boolean)
+  // exerciess (boolean)
+  // healthyDiet (boolean)
+
+});
